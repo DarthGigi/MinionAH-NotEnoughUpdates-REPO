4f23aa2543c9843d2ec4430062eb857e151e6b77
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-12-14 16:57:03 +0100

    fix: skip non-json files

--- scripts/update-recipes.ts
+++ scripts/update-recipes.ts
@@ -19,35 +19,40 @@ async function formatRecipe(recipe: string[]) {
   return output;
 }
 
-const output: { 
+const output: {
   [key: string]: {
     name: string;
     recipe: any;
-    wiki?: string[]
-  }
+    wiki?: string[];
+  };
 } = {};
 const items = await readdir("../items");
 
 for (const item of items) {
+  if (!item.endsWith(".json")) {
+    console.log(`Skipping non-json file: ${item}`);
+    continue;
+  }
+
   const itemData = await Bun.file(`../items/${item}`).json();
   if (itemData.recipe) {
     output[itemData.internalname as string] = {
       name: itemData.displayname.replace(/ยง./g, ""),
       recipe: await formatRecipe(Object.values(itemData.recipe)),
-      wiki: itemData.info,
+      wiki: itemData.info
     };
   }
 
-  const foundRecipes = itemData.recipes?.filter(
-    (recipe: { type: string; }) => recipe?.type === "npc_shop"
-  );
+  const foundRecipes = itemData.recipes?.filter((recipe: { type: string }) => recipe?.type === "npc_shop");
   if (foundRecipes && foundRecipes.length) {
     for (const recipe of foundRecipes) {
       const newRecipe = await formatRecipe(recipe.cost);
       const [fileId, amount] = recipe.result.split(":");
       const id = recipe.result.split(";")[0];
 
-      const itemData = await Bun.file(`../items/${fileId}.json`).json().catch(() => null);
+      const itemData = await Bun.file(`../items/${fileId}.json`)
+        .json()
+        .catch(() => null);
       if (!itemData) {
         console.log(`Item data for ${fileId} not found, skipping...`);
         continue;
@@ -56,7 +61,7 @@ for (const item of items) {
       output[id] = {
         name: itemData?.displayname.replace(/ยง./g, ""),
         recipe: { ...newRecipe, count: parseInt(amount ?? 1) ?? 1 },
-        wiki: itemData?.info,
+        wiki: itemData?.info
       };
     }
   }
@@ -71,8 +76,6 @@ async function getItemId(NEUId: string) {
   return itemData.internalname.replace("-", ":");
 }
 
-const sorted = Object.fromEntries(
-  Object.entries(output).sort(([a], [b]) => a.localeCompare(b))
-);
+const sorted = Object.fromEntries(Object.entries(output).sort(([a], [b]) => a.localeCompare(b)));
 
 await Bun.write("../recipes/recipes.json", JSON.stringify(sorted, null, 2));
